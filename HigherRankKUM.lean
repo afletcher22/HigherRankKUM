31d5511c9376cdef33115f497c01bd3fb222be5a
afletcher22 <austenf22@gmail.com>
2026-09-17 10:13:07 -0700

    chore: export forced pair relation product bridge

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -40,6 +40,7 @@ import HigherRankKUM.AdmissiblePairCycle
 import HigherRankKUM.PairCycleCardinality
 import HigherRankKUM.PairCycleObstruction
 import HigherRankKUM.PairRelationOrbitGauge
+import HigherRankKUM.PairRelationForcedProduct
 import HigherRankKUM.PairBlockPartition
 import HigherRankKUM.PairBlockPartitionReplace
 import HigherRankKUM.FundamentalCircuitExchange
