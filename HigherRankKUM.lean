ca73b3be3633ddb33ac3949c63a6e4ff84944602
afletcher22 <austenf22@gmail.com>
2026-09-16 23:19:48 -0700

    chore: export forced composition invariance

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -30,6 +30,7 @@ import HigherRankKUM.BinaryRelationParity
 import HigherRankKUM.BinaryRelationRelabel
 import HigherRankKUM.BinaryRelationTriangleParity
 import HigherRankKUM.BinaryRelationGauge
+import HigherRankKUM.BinaryRelationForcedCommutative
 import HigherRankKUM.PairCycle
 import HigherRankKUM.PairCycleFundamentalCircuit
 import HigherRankKUM.PairCycleTriangle
