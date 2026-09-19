c5188a5bd0fcbf6d6abfe37a3b8b95af4e90f002
afletcher22 <austenf22@gmail.com>
2026-09-19 12:40:48 -0700

    build: include dangerous branch factors

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -81,3 +81,4 @@ import HigherRankKUM.Rank4.GcdTwoClosurePropagation
 import HigherRankKUM.Rank4.GcdTwoRankTwoTight
 import HigherRankKUM.Rank4.SixElementBoundary
 import HigherRankKUM.Rank4.DangerousCore
+import HigherRankKUM.Rank4.DangerousBranchFactors
