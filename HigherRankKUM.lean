63d0e124d85c869aa0ac8fb1fd7b318b534b23b7
afletcher22 <austenf22@gmail.com>
2026-09-19 12:42:30 -0700

    build: include t3 dangerous geometry

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -82,3 +82,4 @@ import HigherRankKUM.Rank4.GcdTwoRankTwoTight
 import HigherRankKUM.Rank4.SixElementBoundary
 import HigherRankKUM.Rank4.DangerousCore
 import HigherRankKUM.Rank4.DangerousBranchFactors
+import HigherRankKUM.Rank4.DangerousBranchT3Geometry
