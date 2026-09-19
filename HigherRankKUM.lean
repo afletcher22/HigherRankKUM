96942394e4f80630d840f2c3c7d0aacc34824cf7
afletcher22 <austenf22@gmail.com>
2026-09-19 10:31:02 -0700

    feat: formalize dangerous-core ranks

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -80,3 +80,4 @@ import HigherRankKUM.Rank4.GcdTwoCrossRepairParity
 import HigherRankKUM.Rank4.GcdTwoClosurePropagation
 import HigherRankKUM.Rank4.GcdTwoRankTwoTight
 import HigherRankKUM.Rank4.SixElementBoundary
+import HigherRankKUM.Rank4.DangerousCore
