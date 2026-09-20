5706def43af670c84774f9ebd439d739d70ab506
afletcher22 <austenf22@gmail.com>
2026-09-20 15:48:33 -0700

    build: activate t0 two-deletion robustness

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -83,6 +83,7 @@ import HigherRankKUM.Rank4.GcdTwoCrossRepairParity
 import HigherRankKUM.Rank4.GcdTwoClosurePropagation
 import HigherRankKUM.Rank4.GcdTwoRankTwoTight
 import HigherRankKUM.Rank4.T0Deletion
+import HigherRankKUM.Rank4.T0TwoDeletion
 import HigherRankKUM.Rank4.SixElementBoundary
 import HigherRankKUM.Rank4.DangerousCore
 import HigherRankKUM.Rank4.CyclicWindowFour
