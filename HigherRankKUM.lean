3b31bb2df53316822c38c6af70866a3b95c06a77
afletcher22 <austenf22@gmail.com>
2026-09-21 12:07:07 -0700

    build: activate saturated-flat good-edge geometry

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -115,3 +115,5 @@ import HigherRankKUM.Rank4.DangerousScheduleNormalization
 import HigherRankKUM.Rank4.DangerousScheduleSymbolicBasis
 import HigherRankKUM.Rank4.DangerousScheduleSymbolicDistanceTwo
 import HigherRankKUM.Rank4.DangerousScheduleSymbolicEndpoint
+
+import HigherRankKUM.Rank4.SaturatedFlatGood
