f40757ff381077a4c5aac8b22c067d587a295aae
afletcher22 <austenf22@gmail.com>
2026-09-21 01:13:39 -0700

    build: activate three-run cap

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -95,6 +95,7 @@ import HigherRankKUM.Rank4.CyclicFlatCapacity
 import HigherRankKUM.Rank4.T0SaturatedCap
 import HigherRankKUM.Rank4.T0FixedEState
 import HigherRankKUM.Rank4.T0FixedELocalGeometry
+import HigherRankKUM.Rank4.T0FixedEThreeRunCap
 import HigherRankKUM.Rank4.CyclicIndexArithmetic
 import HigherRankKUM.Rank4.CyclicPigeonhole
 import HigherRankKUM.Rank4.FiniteSchedule
