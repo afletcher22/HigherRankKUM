f7341e2fdbeb448b0d55f95e165aab94925b4130
afletcher22 <austenf22@gmail.com>
2026-09-21 12:53:43 -0700

    build: activate saturated six-defect certification

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -98,6 +98,7 @@ import HigherRankKUM.Rank4.CyclicFlatCapacity
 import HigherRankKUM.Rank4.T0SaturatedCap
 import HigherRankKUM.Rank4.SaturatedSixDefectSchedule
 import HigherRankKUM.Rank4.SaturatedSixDefectGluing
+import HigherRankKUM.Rank4.SaturatedSixDefectCertification
 import HigherRankKUM.Rank4.SaturatedFlatRankThree
 import HigherRankKUM.Rank4.SaturatedFourWindowDefect
 import HigherRankKUM.Rank4.T0FixedEState
