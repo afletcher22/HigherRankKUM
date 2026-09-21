662b48514a1dd6cab5c8c40dd4f7330241630ea1
afletcher22 <austenf22@gmail.com>
2026-09-20 20:10:14 -0700

    build: activate strict 4k spanning cap

--- HigherRankKUM.lean
+++ HigherRankKUM.lean
@@ -90,6 +90,7 @@ import HigherRankKUM.Rank4.CyclicWindowFour
 import HigherRankKUM.Rank4.BlockerCycle
 import HigherRankKUM.Rank4.CyclicFlatCapacity
 import HigherRankKUM.Rank4.T0SaturatedCap
+import HigherRankKUM.Rank4.StrictFourKSpanningCap
 import HigherRankKUM.Rank4.CyclicIndexArithmetic
 import HigherRankKUM.Rank4.CyclicPigeonhole
 import HigherRankKUM.Rank4.FiniteSchedule
